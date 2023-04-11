82f6e12508c5cd33aa44915571465e4c1e6cfa88
gmqiyue <gmqiyue@gmail.com>
2023-04-11 13:03:56 +0800

    format main.tsx

--- main.tsx
+++ main.tsx
@@ -1,21 +1,21 @@
 /** @jsx h */
 
-import blog, { ga } from 'blog';
+import blog, { ga } from "blog";
 
-const GA_KEY = 'GOOGLE_ANALYTICS_ID';
+const GA_KEY = "GOOGLE_ANALYTICS_ID";
 
 blog({
-  title: '柒月的博客',
-  description: '简简单单，如此便好',
-  author: '柒月',
-  avatarClass: 'full',
-  avatar: './images/gmqiyue.png',
-  favicon: './images/gmqiyue.png',
-  cover: './images/background.jpg',
-  lang: 'zh',
+  title: "柒月的博客",
+  description: "简简单单，如此便好",
+  author: "柒月",
+  avatarClass: "full",
+  avatar: "./images/gmqiyue.png",
+  favicon: "./images/gmqiyue.png",
+  cover: "./images/background.jpg",
+  lang: "zh",
   links: [
-    { title: 'Email', url: 'mailto:gmqiyue@gmail.com' },
-    { title: 'GitHub', url: 'https://github.com/gmqiyue' },
+    { title: "Email", url: "mailto:gmqiyue@gmail.com" },
+    { title: "GitHub", url: "https://github.com/gmqiyue" },
   ],
   style: `.home header + div > :first-child {border: none;}`, // 隐藏文章列表与Header之间的上边框
   middlewares: [
